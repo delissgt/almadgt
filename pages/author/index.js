0b5c02d4abdfa31f2438797f8df66ace845aabc3
delissgt <delissgt@gmail.com>
2022-06-17 17:16:23 -0500

    Stylish Added Images

--- pages/author/index.js
+++ pages/author/index.js
@@ -1,13 +1,21 @@
 import {PageHeader, Card, Divider, Timeline, Button} from "antd";
 import {GithubOutlined, LinkedinOutlined, MailOutlined} from '@ant-design/icons';
 
+import Image from "next/image";
+
+import imageLeaves from '/public/img/llleaves.svg'
+import line from '/public/img/llline.svg';
+
 
 function AuthorPage (props) {
   return (
-      <>
-        <PageHeader title={"About me"}/>
-        <Card bordered={false}>
-          <Card.Grid hoverable={false} bordered={false} style={{ textAlign: 'center'}}>
+      <div>
+
+        {/*<img src={imageLeaves.src} />*/}
+        {/*style={{background: #fff0 }}*/}
+        <PageHeader title={"About me"}  />
+        <Card bordered={false} style={{background: "#fff0" }} >
+          <Card.Grid hoverable={false} bordered={false} style={{ textAlign: 'center' }}>
             hi, I&apos;m Delia
           </Card.Grid>
           <Card.Grid bordered={false}>
@@ -19,10 +27,11 @@ function AuthorPage (props) {
                   <Button key='mail' shape={'circle'} icon={<MailOutlined />}/>
                 ]}
             >
-              my photo
+              <Image src={line}/>
             </Card>
           </Card.Grid>
-          <Card.Grid hoverable={false} style={{ textAlign: 'center'}} bordered={false}>
+          {/*<Image src={imageLeaves} width={"100%"} height={"100%"} layout={"fill"} objectFit={"cover"} quality={100}/>*/}
+          <Card.Grid hoverable={false} style={{ textAlign: 'center', background: "#fff0"}} bordered={false}>
             I&apos;m Software Engineer
             I&apos;m used to work in Linux, also I enjoy frontend development and  it&apos;s improvement
             through UX. Other relevant fields for me are Data Bases and Testing, I would like to develop
@@ -33,7 +42,9 @@ function AuthorPage (props) {
           </Card.Grid>
         </Card>
         {/*<Divider/>*/}
-        <Card bordered={false}>
+
+        <Card bordered={false} style={{background: "#fff0" }} >
+          <Image src={imageLeaves} width={"100%"} height={"100%"} layout={"fill"} objectFit={"cover"} quality={100}/>
           <PageHeader
               title={"My experience"}
           />
@@ -54,10 +65,16 @@ function AuthorPage (props) {
             </Timeline.Item>
             <Timeline.Item label={'Gamification applied on System for Learning POO with focus in usage for people with motricity disability - Thesis degree- 02/2020'}>
               - Creation of backed using Python and Flask <br/>
-              - Design of Postgrest Data Base <br/>
-              - Creation of Frontend  <br/>
+              - Design of Relational Data Base with Postgrest <br/>
+              - Creation of Frontend using JavaScript, React, Antd <br/>
+              - Backend was Dockerized for easy deployment<br/>
             </Timeline.Item>
-            <Timeline.Item label={'Bentel frontend'}> description</Timeline.Item>
+            <Timeline.Item label={'Béntel frontend'}>
+              - Creation of various React widgets
+              - Improvement of loading data displayed <br/>
+              - Update Configuration for different clients site using react, jquery technologies <br/>
+              - Contribute in backend adjustments and UX design <br/>
+            </Timeline.Item>
 
           </Timeline>
         </Card>
@@ -65,7 +82,6 @@ function AuthorPage (props) {
           <PageHeader
               title={"Tecnologies I have used"}
           />
-
         </Card>
         <Card>
           <PageHeader
@@ -73,7 +89,7 @@ function AuthorPage (props) {
 
           />
         </Card>
-      </>
+      </div>
   )
 }
 
